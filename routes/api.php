cbcfa2311280138ca77183bbf12f1ade4d35b15d
myrg15 <merishield@gmail.com>
2023-12-03 14:28:14 +0100

    feat: create route cruds users, games, parties, message

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//CRUD USERS
+
+// AUTH
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/profile', [AuthController::class, 'profile']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+//CRUD GAMES
+Route::post('/create', [GamesController::class, 'createParties']);
+Route::get('/search', [GamesController::class, 'searchParties']);
+Route::put('/update/{gameId}', [GamesController::class, 'update']);
+Route::delete('/delete/{gameId}',[GamesController::class, 'delete']);
+
+//CRUD PARTIES
+Route::post('/join/{gameId}', [PartiesController::class,'join']);
+Route::post('/leave/{gameId}', [PartiesContoller::class, 'leave']);
+
+//CRUD MESSAGE
+Route::post('/send-message/{gameId}', [MessageContoller::class, 'send-message']);
+//visualizar todos los mensajes en una partida
+Route::get('/view-message/{gameId}',[MessageContoller::class,'allMessageParties']);
+
+
+
+
+
+
+
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
