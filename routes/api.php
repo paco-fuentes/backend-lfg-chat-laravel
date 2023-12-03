beade388b6fa560d0473c39a0703f081d1313c96
myrg15 <merishield@gmail.com>
2023-12-03 17:07:32 +0100

    feat:added middleware by path groups

--- routes/api.php
+++ routes/api.php
@@ -14,47 +14,55 @@ use Symfony\Component\HttpFoundation\Response;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//RUTA DE COMPROBACION
 Route::get('/api', function (Request $request) {
     return response()->json(
         [
             "success" => true,
             "message" => "Healthcheck ok"
         ],
-        Response::HTTP_OK //code status 200
+        Response::HTTP_OK
     );
 });
 
 //CRUD USERS
 
 // AUTH
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/profile', [AuthController::class, 'profile']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group([
+'middleware' =>['auth: sanctum']
+], function() {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 //CRUD GAMES
-Route::post('/create', [GamesController::class, 'createParties']);
-Route::get('/search', [GamesController::class, 'searchParties']);
-Route::put('/update/{gameId}', [GamesController::class, 'update']);
-Route::delete('/delete/{gameId}',[GamesController::class, 'delete']);
+Route::group([
+'middleware' => ['auth: sanctum']    
+], function() {
+    Route::post('/create', [GamesController::class, 'createParties']);
+    Route::get('/search', [GamesController::class, 'searchParties']);
+    Route::put('/update/{gameId}', [GamesController::class, 'update']);
+    Route::delete('/delete/{gameId}',[GamesController::class, 'delete']);
+});
 
 //CRUD PARTIES
-Route::post('/join/{gameId}', [PartiesController::class,'join']);
-Route::post('/leave/{gameId}', [PartiesContoller::class, 'leave']);
+Route::group([
+'middleware' => ['auth: sanctum']
+], function(){
+    Route::post('/join/{gameId}', [PartiesController::class,'join']);
+    Route::post('/leave/{gameId}', [PartiesContoller::class, 'leave']);
+});
 
 //CRUD MESSAGE
-Route::post('/send-message/{gameId}', [MessageContoller::class, 'send-message']);
-//visualizar todos los mensajes en una partida
-Route::get('/view-message/{gameId}',[MessageContoller::class,'allMessageParties']);
-
-
-
-
-
-
-
-
+Route::group([
+    'middleware' => ['auth: sanctum']
+], function(){
+    Route::post('/send-message/{gameId}', [MessageContoller::class, 'send-message']);
+    //visualizar todos los mensajes en una partida
+    Route::get('/view-message/{gameId}',[MessageContoller::class,'allMessageParties']);
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
