be18a5749c24ee893def6e1461a2afed5c42c0f2
myrg15 <merishield@gmail.com>
2023-12-03 17:17:02 +0100

    feat: create folder controller

--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,6 @@ Route::group([
 });
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
