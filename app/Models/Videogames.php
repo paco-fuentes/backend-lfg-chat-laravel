0c4a971ce88797a239941495e9fd099bd82adbdb
Christian <christianperez.rbb@gmail.com>
2023-12-05 00:43:24 +0100

    feat: create Videogames Model.

---
+++ app/Models/Videogames.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Videogames extends Model
+{
+    use HasFactory, HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'title',
+        'year',
+        'img_url',
+        'genre',
+        'is_active',
+    ];
+
+    public function party_rooms():HasMany
+    {
+        return $this->hasMany(PartyRooms::class);
+    }
+}
