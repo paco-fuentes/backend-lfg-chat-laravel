50358b577a9ab9b93dbb006413d4930575ffd0fd
Christian <christianperez.rbb@gmail.com>
2023-12-07 13:45:25 +0100

    fix: Videogames factory fixed, now is running.

--- database/factories/VideogamesFactory.php
+++ database/factories/VideogamesFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Videogames>
@@ -13,10 +16,10 @@ class VideogamesFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->title,
+            'title' => $this->faker->name,
             'year' => $this->faker->year,
             'img_url' => $this->faker->imageUrl,
-            'genre' => $this->faker->randomElement(['MMORPG', 'Action', 'Adventure', 'RPG', 'FPS']),
+            'genre' => $this->faker->randomElement(['unknown', 'Action', 'Adventure', 'RPG', 'FPS']),
             'is_active' => $this->faker->boolean,
             //
         ];
