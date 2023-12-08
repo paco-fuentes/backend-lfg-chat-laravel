4e459ff46841922dcd3fc8aa97b8690ef6f158d9
myrg15 <merishield@gmail.com>
2023-12-08 19:38:48 +0100

    feat: modified controller videogame

--- app/Http/Controllers/VideogamesController.php
+++ app/Http/Controllers/VideogamesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Videogames;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
