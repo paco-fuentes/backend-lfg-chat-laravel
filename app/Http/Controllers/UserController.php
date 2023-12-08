220e159f41d37761cabc67835f7416ad7178c3e3
Christian <christianperez.rbb@gmail.com>
2023-12-08 23:54:47 +0100

    feat: create user profile endpoint.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -121,4 +121,30 @@ class UserController extends Controller
             );
         }
     }
+
+    public function profile(Request $request)
+    {
+        try {
+            $user = auth() -> user();
+
+            return response()-> json(
+                [
+                    "success" => true,
+                    "message" => "User profile",
+                    "data" => $user,
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()-> json(
+                [
+                    "success" => false,
+                    "message" => "Error getting profile",
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
