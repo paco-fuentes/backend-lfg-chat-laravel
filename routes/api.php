0574fe637cdfb3b41e08ce1585c791a88082707a
Christian <christianperez.rbb@gmail.com>
2023-12-09 01:14:41 +0100

    feat: Create update profile route.

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::post('/login', [UserController::class, 'login']);
 // function() {
     Route::middleware("auth:sanctum")->get('/profile', [UserController::class, 'profile']);
     Route::middleware("auth:sanctum")->post('/logout', [UserController::class, 'logout']);
+    Route::middleware("auth:sanctum")->put('/update', [UserController::class, 'updateProfile']);
 // }
 // );
 
