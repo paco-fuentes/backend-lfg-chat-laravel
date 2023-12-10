d07338bea8355fcfe89bfd166332135114e3c6e6
Christian <christianperez.rbb@gmail.com>
2023-12-10 03:25:34 +0100

    feat: create get message endpoint.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -52,4 +52,52 @@ class MessageController extends Controller
             );
         }
     }
+
+    public function chatRoom(Request $request)
+    {
+        try {
+            $user = auth()->user();
+            $partyRoom_id = $request->input('party_id');
+            $partyMember = PartyMember::query()->where("user_id", $user->id)->where("party_id", $partyRoom_id)->first();
+
+            if (!$partyMember) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "You are not a member of this chat"
+                    ],
+                    Response::HTTP_UNAUTHORIZED
+                );
+            }
+
+            $chatRoom = Message::query()->where("party_id", $partyRoom_id)->get();
+
+            if ($chatRoom->isEmpty()) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "This chat is empty"
+                    ],
+                    Response::HTTP_OK
+                );
+            }
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Chat obtained",
+                    "data" => $chatRoom
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error getting chat"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
